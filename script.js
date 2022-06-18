0ec4849ddef7a6ca0da7d797c32fb8f3617d627b
“SidhraFernando-Plant <s3873756@student.rmit.edu.au>
2022-06-18 21:18:22 +1000

    Add functionality to operation keys (+, -, *, /)

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+let operand1;
+let operand2;
+let operation;
+
 function typeNumber(num) {
   let screenTxtNode = document.getElementById('screen-text');
-  screenTxtNode.innerText += num;
+  if (screenTxtNode.innerText === '0') {
+    screenTxtNode.innerText = num;
+  } else {
+    screenTxtNode.innerText += num;
+  }
+}
+
+function typeOperation(op) {
+  let screenTxtNode = document.getElementById('screen-text');
+  operand1 = parseInt(screenTxtNode.innerText);
+  screenTxtNode.innerText = '0';
+  operation = op;
 }
 
 let numberKeyNodes = document.querySelectorAll('.number-btn');
 numberKeyNodes.forEach((key) =>
   key.addEventListener('click', () => typeNumber(key.textContent))
+);
+
+let opKeyNodes = document.querySelectorAll('.op-btn');
+opKeyNodes.forEach((key) =>
+  key.addEventListener('click', () => typeOperation(key.textContent))
 );
